200cdbe0f8f8db011941f7f2d06666f45ccf8104
zhaijie1 <zhaijie1@kingsoft>
2019-03-15 17:14:00 +0800

    :art: formwork

--- src/test/java/other/designpattern/action/formwork/AbstractSort.java
+++ src/test/java/other/designpattern/action/formwork/AbstractSort.java
@@ -1,5 +1,13 @@
 package other.designpattern.action.formwork;
 
-public class AbstractSort {
+public abstract class AbstractSort {
+    protected abstract void sort(int[] array);
 
+    public void showSortResult(int[] array){
+        this.sort(array);
+        System.out.print("排序结果:");
+        for(int i = 0;i<array.length;i++){
+            System.out.printf("%3s",array[i]);
+        }
+    }
 }
