b382283b9a42b132cf16a1ab22f22d377d74b4b1
Zaki Argen <argenzaki@gmail.com>
2019-04-24 21:20:25 +0800

    :no_see_evil: update project gradle

---
+++ src/main/java/com/zaki/mapper/CountryMapper.java
@@ -0,0 +1,17 @@
+package com.zaki.mapper;
+
+import com.zaki.model.Country;
+
+public interface CountryMapper {
+    int deleteByPrimaryKey(String code);
+
+    int insert(Country record);
+
+    int insertSelective(Country record);
+
+    Country selectByPrimaryKey(String code);
+
+    int updateByPrimaryKeySelective(Country record);
+
+    int updateByPrimaryKey(Country record);
+}
