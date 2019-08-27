3ccdc645b33d64142f700f735203f9e38c3ee1bc
argen <argenzaki@gmail.com>
2019-08-27 20:17:15 +0800

    :art: update project

--- src/main/java/com/zaki/Application.java
+++ src/main/java/com/zaki/Application.java
@@ -1,10 +1,9 @@
 package com.zaki;
 
-import org.activiti.spring.boot.SecurityAutoConfiguration;
-import org.mybatis.generator.api.CommentGenerator;
-import org.mybatis.spring.annotation.MapperScan;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
