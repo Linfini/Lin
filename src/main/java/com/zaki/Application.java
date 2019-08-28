6fbaa286a26f11caa4145ac07456f22d57db6e30
zhaijie1 <zhaijie1@kingsoft>
2019-08-28 10:57:22 +0800

    :art: log

--- src/main/java/com/zaki/Application.java
+++ src/main/java/com/zaki/Application.java
@@ -1,20 +1,28 @@
 package com.zaki;
 
 import org.activiti.spring.boot.SecurityAutoConfiguration;
-import org.mybatis.generator.api.CommentGenerator;
-import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @EnableTransactionManagement
 @ServletComponentScan
 @EnableAsync
 public class Application {
+    static Logger logger = LoggerFactory.getLogger(Application.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+        ConfigurableEnvironment environment = context.getEnvironment();
+        String port = environment.getProperty("server.port");
+        logger.info("application start in localhost:" + port);
     }
 }
