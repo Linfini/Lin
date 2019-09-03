edf3490f0a51b44502699f8d118e7648242511b0
zhaijie1 <zhaijie1@kingsoft>
2019-09-03 16:35:49 +0800

    :art: websocket support

---
+++ gs-messaging-stomp-websocket/complete/src/main/java/hello/WebSocketConfig.java
@@ -0,0 +1,31 @@
+package hello;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.broker.SimpleBrokerMessageHandler;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+/**
+ * 使用更高级别的子协议 STOMP来进行消息传递
+ * */
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        /**
+         * topic 开头的使用build-in 消息代理
+         * */
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/gs-guide-websocket").withSockJS();
+    }
+
+}
